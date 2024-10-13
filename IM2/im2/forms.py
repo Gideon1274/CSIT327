cca6d885c50d0f3ad5f08e1787cd478ba9e64340
Gideon1274 <155309336+Gideon1274@users.noreply.github.com>
2024-10-14 02:01:43 +0800

    Delete IM2/im2 directory

--- IM2/im2/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-    name = forms.CharField(label = 'Your name', max_length = 100)
-    email = forms.EmailField(label = 'Your email')
-    message = forms.CharField(widget=forms.Textarea, label = 'Messsage')
-    
